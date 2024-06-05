7ec4b375a92e6377e06275fb860d6e7c00744d15
Drekstord <drekstord@outlook.fr>
2024-06-05 16:25:33 +0200

    Ajout du seul exo que j'ai réussi à faire fonctionner entièrement, le 2 crash alors que le 1 fonctionne qu'avec des int. Il y a du chemin

--- JS/Exercice 3/exo3.js
+++ JS/Exercice 3/exo3.js
@@ -1,6 +1,12 @@
 const motLePlusLong = (phrase) => {
 
-    
+    return phrase.split(" ").reduce((x, y) => {
+        return y.length >= x.length ? y : x;
+    }, "");
 
+};
 
-}
+console.log(motLePlusLong('a b c d e fgh'));
+console.log(motLePlusLong('one two three'));
+console.log(motLePlusLong('red blue grey'));
+console.log(motLePlusLong("J'ai envie d'essayer aevc une phrase bien plus longue que le mot anticonsitutionnellement, en partant du principe qu'il n'y ait pas de faute "))
