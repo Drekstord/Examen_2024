294c09e9d6973eb8f23868ad723083c506e57521
Drekstord <drekstord@outlook.fr>
2024-06-05 15:43:56 +0200

    Exo 1 fonctionne qu'avec des int

--- JS/Exercice 1/exo1.js
+++ JS/Exercice 1/exo1.js
@@ -1,10 +1,22 @@
 
+const calculer_pourboire = (montant, qualite) => {
 
-const liste_pourboires = {
+    const liste_pourboires = {
     Terrible : 0,
     Poor : 5,
     Good : 10,
     Great : 15,
-    Excellent : 20,
-}
+    Excellent : 20
+    };
 
+    if (!(qualite) in liste_pourboires) {
+        return "Rating not recognised";
+    }
+    
+    const pourcent_pourboire = qualite / 100 ;
+    const pourboire = montant * qualite ;
+
+    return pourboire
+}  
+
+console.log(calculer_pourboire(20, Terrible));
